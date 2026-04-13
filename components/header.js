711ec3e4c137c51cb230f8854c1c5585c08c2c55
Jisell <jisellcastov@gmail.com>
2026-04-13 11:58:27 -0500

    Avance header: iconos

--- components/header.js
+++ components/header.js
@@ -30,8 +30,31 @@ const input = document.createElement("input");
 input.type = "text";
 input.placeholder = "Search products and more.....";
 
+const btnBuscar = document.createElement("button");
+const lupaImg = document.createElement("img");
+lupaImg.src = "assets/images/Header/Lupa.png";
+lupaImg.alt = "buscar";
+btnBuscar.appendChild(lupaImg);
+
 searchBox.appendChild(input);
+searchBox.appendChild(btnBuscar);
 topHeader.appendChild(searchBox);
+
+// Iconos
+const icons = document.createElement("div");
+icons.classList.add("icons");
+
+const carrito = document.createElement("img");
+carrito.src = "assets/images/Header/Carrito.png";
+carrito.alt = "carrito";
+
+const favoritos = document.createElement("img");
+favoritos.src = "assets/images/Header/Favoritos.png";
+favoritos.alt = "favoritos";
+
+icons.appendChild(carrito);
+icons.appendChild(favoritos);
+topHeader.appendChild(icons);
 
 // Agrego la fila de arriba al header
 header.appendChild(topHeader);
